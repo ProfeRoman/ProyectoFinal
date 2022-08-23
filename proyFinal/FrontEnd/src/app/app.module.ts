1f019c91733587194c0528db76e05df8fc4639df
ProfeRoman <rscollo@hotmail.com>
2022-08-23 10:13:16 -0300

    vv

--- proyFinal/FrontEnd/src/app/app.module.ts
+++ proyFinal/FrontEnd/src/app/app.module.ts
@@ -14,6 +14,7 @@ import { ProyectoComponent } from './components/proyecto/proyecto.component';
 import { FooterComponent } from './components/footer/footer.component';
 import { IniciarSesionComponent } from './components/iniciar-sesion/iniciar-sesion.component';
 import { PortfolioComponent } from './components/portfolio/portfolio.component';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
@@ -33,7 +34,9 @@ import { PortfolioComponent } from './components/portfolio/portfolio.component';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    NgCircleProgressModule.forRoot({})
+    NgCircleProgressModule.forRoot({}),
+    ReactiveFormsModule
+
   ],
   providers: [],
   bootstrap: [AppComponent]
